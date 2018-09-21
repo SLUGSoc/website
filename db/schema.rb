461638a77e9b54a69e5039fb55e8276639d64573
Simon Fish <sfish@ukcloud.com>
2018-09-21 20:49:44 +0100

    Fix platform icons in views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_190239) do
+ActiveRecord::Schema.define(version: 2018_09_21_192300) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "datetime"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2018_09_21_190239) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
-    t.string "platform"
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
