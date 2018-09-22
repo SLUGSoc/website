848bbeb347ed88de3b490b6f154833c910abb6d6
Simon Fish <sfish@ukcloud.com>
2018-09-22 11:10:45 +0100

    Add initial event cards

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ApplicationRecord
   has_many :game_event_relations, dependent: :destroy
   has_many :games, through: :game_event_relations
+  def self.all_future
+    all.select { |event| event.datetime.future? }
+  end
+
+  def facebook_event_link
+    "https://facebook.com/#{facebook_event_id}"
+  end
 end
