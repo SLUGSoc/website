e02cfb77940eee41705625449dc975d102af1bad
Simon Fish <sfish@ukcloud.com>
2018-09-23 17:20:29 +0100

    Ensure events are chronologically ordered

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   has_many :game_event_relations, dependent: :destroy
   has_many :games, through: :game_event_relations
+  default_scope { order('datetime ASC, id ASC') }
   def self.all_future
     all.select { |event| event.datetime.future? }
   end
