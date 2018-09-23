f9e77fb5d5a054eb8d0341d9564e2e76c50ed2f1
Simon Fish <sfish@ukcloud.com>
2018-09-23 17:45:53 +0100

    Add favicon, page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def title
+    if content_for?(:title)
+      # allows the title to be set in the view by using t(".title")
+      content_for :title + " - #{t(:site_name)}"
+    else
+      # look up translation key based on controller path, action name and .title
+      # this works identical to the built-in lazy lookup
+      t("#{ controller_path.tr('/', '.') }.#{ action_name }.title", default: :site_name)
+    end
+  end
 end
